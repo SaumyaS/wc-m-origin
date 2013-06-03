fa55ec7de7bc4416171379134060675f26a14248
unknown <ss012701@H1325489.hsi.hughessupply.com>
2013-06-03 17:41:21 -0400

    shopping cart

--- scripts/pages/orderItemDisplay.ts
+++ scripts/pages/orderItemDisplay.ts
@@ -43,6 +43,57 @@ $$(".order-confirm"){
 
 
 		}
+		$$(".order-product-last"){
+			$("./div[1]"){
+				add_class("_sku")
+			}
+			$("./div[2]"){
+				add_class("_delete")
+			}
+			$("./div[3]"){
+				add_class("_delete")
+			}
+			$("./div[4]"){
+				add_class("_remove")
+			}
+			$("./div[5]"){
+				add_class("_delete")
+			}
+			$("./div[6]"){
+				add_class("_delete")
+			}
+		}
+
+		$$("#cart-total"){
+			$("./div[1]"){
+				attributes(style: "", class: "_promo")
+			}
+			$("./div[2]"){
+				attributes(style: "", class: "_shipping")
+				insert_after("div", class: "_tax")
+			}
+			$("./div[3]"){
+				insert("h2", "Estimated Tax")
+				move_here("../div[@class='_shipping']/div")
+				$("./div"){
+					attribute("style", "")
+
+					$("./label[2]"){
+						insert_before("br")
+					}
+					$("./label[3]"){
+						insert_before("br")
+					}
+					$("./label[4]"){
+						insert_before("br")
+					}
+					$("./font"){
+						insert_before("br")
+					}
+
+				}
+			}
+		}
 		
 		
 	}
