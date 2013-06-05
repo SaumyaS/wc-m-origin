f3bb4f5c60c53fcea9804358df3d449a9d42027d
unknown <ss012701@H1325489.hsi.hughessupply.com>
2013-06-05 16:32:29 -0400

    contact us

---
+++ scripts/pages/contactus.ts
@@ -0,0 +1,14 @@
+$$("#ad_17601"){
+	move_here("//div[@id='_icons_bar']")
+}
+
+$("//*[@id='main_content_wrapper']/div/div"){
+	$("./div"){
+		attribute("class", "_contact")
+	}
+	
+}
+
+$("//*[@id='WC_ContentAreaESpot_div_22_1']/div/div/div/div"){
+	attribute("style", "padding: 10px; background-color: rgb(202, 202, 202); width: 97%;")
+}
