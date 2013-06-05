33121965507e186c18cb5dfb5b09eec5708561c8
unknown <ss012701@H1325489.hsi.hughessupply.com>
2013-06-05 13:58:37 -0400

    test

--- scripts/pages/location.ts
+++ scripts/pages/location.ts
@@ -7,36 +7,16 @@ $("//*[@id='main_content_wrapper']/div/div"){
 		attribute("class", "_location")
 	}
 
+
+
 	$("//*[@id='WC_ContentAreaESpot_div_1_18101']/hr"){
 		insert_after("div", class: "_map")
 	}
 
-	$("./div[@class='_location']"){
-		$("./div[@id='WC_ContentAreaESpot_div_1_18101']"){
-			$("./div[@class='_map']"){
-				insert("div", id: "_localContainer", data-ur-set: "tabs", "Store Locator")
+	
+}
 
-
-				$("./div[@id='_localContainer']"){
-					insert("div", id: "map_details", data-ur-set: "map")
-					$("./div[@id='map_details']"){
-						insert("div", data-ur-map-component: "user_location")
-						insert("div", data-ur-map-component: "canvas", id: "map_canvas")
-						insert("div", data-ur-map-component: "address", "501 W Church Street, Orlando, FL")
-						insert("div", data-ur-map-component: "description", id: "_description")
-
-						$$("#_description"){
-							insert("a", href: "www.whitecap.com", "WhiteCap")
-							insert("span", "407-555-6666")
-						}
-					}
-				}
-
-			}
-
-
-
-		}
-	}
+$("//*[@id='WC_ContentAreaESpot_div_22_1']/iframe"){
+	attributes(width: "300px", height: "600px;", scrolling: "no", frameborder: "0")
 }
 
