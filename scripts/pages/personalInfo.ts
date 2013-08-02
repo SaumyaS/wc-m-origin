4e3ab2866eae547d838852ae30de9b9603909451
unknown <ss012701@H1325489.hsi.hughessupply.com>
2013-08-02 11:19:08 -0400

    text box alignment for personal info

--- scripts/pages/personalInfo.ts
+++ scripts/pages/personalInfo.ts
@@ -16,11 +16,20 @@ $("//*[@id='Register']"){
 	$("./div/div[2]/input[@id='WC_UserRegistrationUpdateForm_FormInput_email_In_Register_1']"){
 		insert_after("br")
 	}
+	$("./div/div/label"){
+		insert_before("br")
+	}
+	$("./div/div[1]/input[@id='WC_UserRegistrationUpdateForm_AddressEntryForm_FormInput_address1_1']"){
+		insert_after("br")
+	}
 
 	$("./div/div[1]/input[@id='WC_UserRegistrationAddForm_AddressEntryForm_FormInput_address2_1']"){
 		attribute("style", "margin-left: 114px !important;")
 	}
-
+	
+	$("./div/div[2]"){
+		attribute("style", "float: left;")
+	}
 
 }
 
