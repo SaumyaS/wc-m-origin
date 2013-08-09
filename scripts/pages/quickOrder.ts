f4535f23801ff0e3f02f6bc578bf81334958f2af
unknown <ss012701@H1325489.hsi.hughessupply.com>
2013-08-09 10:51:31 -0400

    changes to quick order

--- scripts/pages/quickOrder.ts
+++ scripts/pages/quickOrder.ts
@@ -4,17 +4,79 @@ $$("#ad_17601"){
 	move_here("//div[@id='_icons_bar']")
 }
 
-$("//*[@id='MQuickOrderForm']/div[1]/div"){
-	$("./div[1]"){
-		attribute("style", "padding-top:7px; padding-right: 3px; float:left")
+
+$$(".account-col-quick-order"){
+	$("./div[@class='Quick-Order-eSpot']"){
+		$("./div[@class='quick-order-po-number']"){
+			insert_after("div", class: "_shipToQO"){
+				move_here("../h3")
+				move_here("../ul[@class='quick-order-orgs orgs']")
+			}
+		}
+		$("./div[@class='_shipToQO']"){
+			insert_after("div", class: "_addressQO"){
+				move_here("../div[@class='erp-shipping-address']")
+				move_here("../div[@class='erp-shipping-address-detail']")
+			}
+			insert_after("hr")
+		}
+		$("./div[@class='_addressQO']"){
+
+		}
+		$("./div[@id='WC_ContentAreaESpot_div_1_18351']"){
+			insert_after("div", class: "_textQO"){
+				move_here("../div[@id='WC_ContentAreaESpot_div_1_18351']/div[2]/h3")
+			}
+			$("./div[2]"){
+				remove_text_nodes()
+
+				$("./iframe"){
+					insert_after("div", "See more help videos in our ")
+				}
+
+				$("./h3"){
+					attributes(style: "display: none;")
+				}
+				$("./br"){
+					remove()
+				}
+			}
+			
+			
+		}
+
+		$("./div[@class='clear_01']"){
+			remove()
+		}
+
 	}
-	$("./div[3]"){
-		attribute("style", "padding-top:7px; padding-right: 5px; float:left")
-	}
-	$("./div[4]"){
-		attribute("style", "float:left")
+
+	$(".//form[@name='MQuickOrderForm']"){
+		attributes(style: "margin-bottom:10px;")
+		$("./div[1]/div"){
+			
+			$("./div[1]"){
+				attribute("style", "padding-top:7px; padding-right: 3px; float:left")
+			}
+			$("./div[3]"){
+				attribute("style", "padding-top:7px; padding-right: 5px; float:left")
+			}
+			$("./div[4]"){
+				attribute("style", "margin-bottom:5px;")
+				$("./input"){
+					attributes(style: "width:149px;")
+				}
+			}
+			$("./div[5]"){
+				insert_after("br")
+			}
+		}
 	}
 }
+
+
+//*[@id="MQuickOrderForm"]
+
 
 $("//*[@id='ad_19601']"){
 	$("./span[2]"){
@@ -27,15 +89,7 @@ $("//*[@id='ad_19601']"){
 	}
 }
 
-$("//*[@id='ad_18351']"){
-	
-	$("./h3"){
-		remove()
-	}
-	insert("h3", "Want to order product quickly and easly?")
-	insert("br")
-	insert("h3", "You can use our Quick Order feature to quickly add products and complete your purchase.")
-}
+
 
 
 	$$("#order_details"){
