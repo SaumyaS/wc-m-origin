8f303b147ef21914e9f9f7a49454073152c9283f
SaumyaS <saumya.proof@gmail.com>
2013-10-11 09:23:57 -0400

    Putting my changes back in

--- scripts/pages/login.ts
+++ scripts/pages/login.ts
@@ -12,7 +12,7 @@ $("/html/body"){
 			$("./div[@class='col-1']"){
 				$("./form"){
 					$("./label[2]"){
-						insert_before("br")
+						# insert_before("br")
 					}
 					$("./input") {
 						add_class("expand")
@@ -65,60 +65,57 @@ $("/html/body"){
 			}
 		}
 		$(".//div[@class='mw_was_tbody']/div"){
-		insert_top("div", class: "_totalPrice", "Total Price: ")
-		insert_top("div", class: "_status", "Status: ")
-		insert_top("div", class: "_purchaseOrder", "Purchase Order: ")
-		insert_top("div", class: "_orderDate", "Order Date: ")
-		insert_top("div", class: "_orderNumber", "Order Number: ")
+			insert_top("div", class: "_totalPrice", "Total Price: ")
+			insert_top("div", class: "_status", "Status: ")
+			insert_top("div", class: "_purchaseOrder", "Purchase Order: ")
+			insert_top("div", class: "_orderDate", "Order Date: ")
+			insert_top("div", class: "_orderNumber", "Order Number: ")
 
-		$("./div[6]"){
-			move_to("../div[1]", position("after"))
-		}
-		$("./div[7]"){
-			move_to("../div[3]", position("after"))
-		}
-		$("./div[9]"){
-			move_to("../div[5]", position ("after"))
-		}
-		$("./div[10]"){
-			move_to("../div[7]", position("after"))
-		}
+			$("./div[6]"){
+				move_to("../div[1]", position("after"))
+			}
+			$("./div[7]"){
+				move_to("../div[3]", position("after"))
+			}
+			$("./div[9]"){
+				move_to("../div[5]", position ("after"))
+			}
+			$("./div[10]"){
+				move_to("../div[7]", position("after"))
+			}
 
-		$("./div[@class='_orderNumber']"){
-			attributes(style: "float: left; font-weight: bold; min-width: 120px;")
-		}
-		$("./div[@class='_orderDate']"){
-			attributes(style: "float: left; font-weight: bold; min-width: 120px;")
-		}
-		$("./div[@class='_purchaseOrder']"){
-			attributes(style: "float: left; font-weight: bold; min-width: 120px;")
-		}
-		$("./div[@class='_status']"){
-			attributes(style: "float: left; font-weight: bold; min-width: 120px;")
-		}
-		$("./div[@class='_totalPrice']"){
-			attributes(style: "float: left; font-weight: bold; min-width: 120px;")
-		}
+			$("./div[@class='_orderNumber']"){
+				attributes(style: "float: left; font-weight: bold; min-width: 120px;")
+			}
+			$("./div[@class='_orderDate']"){
+				attributes(style: "float: left; font-weight: bold; min-width: 120px;")
+			}
+			$("./div[@class='_purchaseOrder']"){
+				attributes(style: "float: left; font-weight: bold; min-width: 120px;")
+			}
+			$("./div[@class='_status']"){
+				attributes(style: "float: left; font-weight: bold; min-width: 120px;")
+			}
+			$("./div[@class='_totalPrice']"){
+				attributes(style: "float: left; font-weight: bold; min-width: 120px;")
+			}
 
-		$("./div[13]"){
-			wrap("div"){
-				attributes(class: "_buttons", style: "margin-bottom: 40px;")
-				move_here("../div[14]")
-				$("./div[1]"){
-					attributes(style: "float: left;")
-				}
-				$("./div[2]"){
-					attributes(style: "float: right;")
+			$("./div[13]"){
+				wrap("div"){
+					attributes(class: "_buttons", style: "margin-bottom: 40px;")
+					move_here("../div[14]")
+					$("./div[1]"){
+						attributes(style: "float: left;")
+					}
+					$("./div[2]"){
+						attributes(style: "float: right;")
+					}
 				}
 			}
 		}
-
+		
 		$("//*[@id='WC_AccountDisplay_links_3']") {
 			add_class("expand")
 		}
-	}
-
-
-
 	}
 }
