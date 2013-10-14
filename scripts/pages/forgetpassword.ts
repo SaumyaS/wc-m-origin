b3499831f869a6b500e297ed478fe8e9cb3cdf29
demetri ganoff <dganoff@gmail.com>
2013-10-14 15:08:48 -0400

    forget password style changes

--- scripts/pages/forgetpassword.ts
+++ scripts/pages/forgetpassword.ts
@@ -1,12 +1,14 @@
 # Moves header icons back to the header
-$$("#ad_17601"){
+$$("#ad_17601") {
 	move_here("//div[@id='_icons_bar']")
 }
 
-$("/html/body"){
-	$("//div[@class='main_container']"){
+$("/html/body") {
+	$("//div[@class='main_container']") {
 		add_class("_forgetPassword")	
-	}
+	}	
+}
 
-	
+$$(".btn") {
+	add_class("expand")
 }
