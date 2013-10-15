8079340ed8c774dafb567cc37362a4b3d343e2aa
SaumyaS <saumya.proof@gmail.com>
2013-10-15 09:12:24 -0400

    Changes on Brands Page

--- scripts/pages/brands.ts
+++ scripts/pages/brands.ts
@@ -15,6 +15,34 @@ $$(".main_wrapper"){
 		$("./h1[2]"){
 			attributes(class: "_bottomTitle")
 		}
+		$("./h2"){
+			attributes(class: "_bottomTitleSub")
+		}
+
+		$("./h1[@class='_topTitle']"){
+			attributes(data-ur-toggler-component: "button", data-ur-id: "topTitle")
+		}
+
+		$("./div[@class='brand-gallery']"){
+			attributes(data-ur-toggler-component: "content", data-ur-id: "topTitle")
+		}
+
+		$("./h1[@class='_bottomTitle']"){
+			attributes(data-ur-toggler-component: "button", data-ur-id: "bottomTitle")
+			insert_after("div", id: "_bottomToggler"){
+				attributes(data-ur-toggler-component: "content", data-ur-id: "bottomTitle")
+				move_here("../h2[@class='_bottomTitleSub']")
+				move_here("../div[@class='all-brands-alpha']")
+			}
+		}
+
+
+
+
+		$("./div[@class='all-brands-alpha']"){
+			
+		}
+
 
 
 
