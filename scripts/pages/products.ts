4a08c0f935c546938200d91d2aa67af1bd96f591
SaumyaS <saumya.proof@gmail.com>
2013-10-16 14:18:01 -0400

    hide no further refinements

--- scripts/pages/products.ts
+++ scripts/pages/products.ts
@@ -7,6 +7,45 @@ $$("#ad_17601"){
 $$(".main_container"){
 	add_class("_products")
 
+	$(".//div[@class='search-sidebar']"){
+
+		$("./h2"){
+			insert_after("div", class:"_resultContent"){
+				insert("div", class: "_tabWidget"){
+					move_here("../../ul")
+				}
+			}
+		}
+
+		attribute("data-ur-set", "toggler")
+		$("./h2"){
+			attributes(data-ur-toggler-component: "button", data-ur-toggler-id: "search")
+		}
+		$("./div"){
+			attributes(data-ur-toggler-component: "content", data-ur-toggler-id: "search")
+		}
+
+		$(".//div[@class='_tabWidget']"){
+			attributes(data-ur-set: "tabs", data-ur-closeable: "true")
+			$("ul[1]"){
+				$("./li/h4"){
+					attributes(data-ur-tabs-component: "button", data-ur-tab-id: "price")
+				}
+				$("./li/ul"){
+					attributes(data-ur-tabs-component: "content", data-ur-tab-id: "price")
+				}
+			}
+			$("ul[2]"){
+				$("./li/h4"){
+					attributes(data-ur-tabs-component: "button", data-ur-tab-id: "mfg")
+				}
+				$("./li/ul"){
+					attributes(data-ur-tabs-component: "content", data-ur-tab-id: "mfg")
+				}
+			}
+		}	
+	}
+
 	$(".//div[@class='search-main']"){
 		$(".//div[@class='search_based']/span[@class='fright']"){
 			$("./select[@id='sortBy']/option"){
@@ -35,60 +74,11 @@ $$(".main_container"){
 	}
 }
 
-$$(".search-sidebar"){
-
-	$("./h2"){
-		insert_after("div", class:"_resultContent")
-	}
-
-	$("./div"){
-		insert("div", class: "_tabWidget")
-	}
 
 
-
-	$$("._tabWidget"){
-		move_here("../../ul")
-	}
-
-	attribute("data-ur-set", "toggler")
-	$("./h2"){
-		attributes(data-ur-toggler-component: "button", data-ur-toggler-id: "search")
-	}
-	$("./div"){
-		attributes(data-ur-toggler-component: "content", data-ur-toggler-id: "search")
-	}
-
-	$$("._tabWidget"){
-		attributes(data-ur-set: "tabs", data-ur-closeable: "true")
-		$("ul[1]"){
-			$("./li/h4"){
-				attributes(data-ur-tabs-component: "button", data-ur-tab-id: "price")
-			}
-			$("./li/ul"){
-				attributes(data-ur-tabs-component: "content", data-ur-tab-id: "price")
-			}
-		}
-		$("ul[2]"){
-			$("./li/h4"){
-				attributes(data-ur-tabs-component: "button", data-ur-tab-id: "mfg")
-			}
-			$("./li/ul"){
-				attributes(data-ur-tabs-component: "content", data-ur-tab-id: "mfg")
-			}
-		}
-	}
-
-	
-}
-
-$$(".search-main"){
-	# $("./div/div/div/ul"){
-	# 	remove()
-	# }
-	$$(".pricingLoginSearch"){
-		$("./a"){
-			attributes(style: "", href: "/webapp/wcs/stores/servlet/AjaxLogonForm?catalogId=11101&myAcctMain=1&langId=-1&storeId=11301", onclick: "")
-		}
+$$(".pricingLoginSearch"){
+	$("./a"){
+		attributes(style: "", href: "/webapp/wcs/stores/servlet/AjaxLogonForm?catalogId=11101&myAcctMain=1&langId=-1&storeId=11301", onclick: "")
 	}
 }
+
