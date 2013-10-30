315caff110be5587eb77dd18af9d4864828a44e1
SaumyaS <saumya.proof@gmail.com>
2013-10-30 17:28:39 -0400

    starting will call

--- scripts/pages/WillCallModalView.ts
+++ scripts/pages/WillCallModalView.ts
@@ -7,3 +7,18 @@
 # }
 
 # //*[@id="will_call_modal_dialog"]/div[2]/div/div[1]/a
+
+$$("#map_canvas"){
+	attributes(style: "display: none;")
+}
+
+$("//a[@title='Cancel']"){
+	$(".."){
+		add_class("_cancelContainer")
+		attributes(style: "")
+	}
+}
+
+# $$(".will_call_map_wrapper"){
+# 	attributes(style: "display:none;")
+# }
