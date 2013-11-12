686767cee5de152fcd2885b2cb4e05afe065242e
SaumyaS <saumya.proof@gmail.com>
2013-11-12 00:54:40 -0500

    Fixing issues with mapping

--- scripts/pages/brands.ts
+++ scripts/pages/brands.ts
@@ -1,53 +1,43 @@
+$("/html"){
+	# Moves header icons back to the header
+	$$("#ad_17601"){
+		move_here("//div[@id='_icons_bar']")
+	}
 
-# Moves header icons back to the header
-$$("#ad_17601"){
-	move_here("//div[@id='_icons_bar']")
-}
+	$$(".main_wrapper"){
+		$("./div[@class='main_container']"){
+			add_class("_brands")
+			attributes(data-ur-set: "toggler")
 
-$$(".main_wrapper"){
-	$("./div[@class='main_container']"){
-		add_class("_brands")
-		attributes(data-ur-set: "toggler")
+			$("./h1[1]"){
+				attributes(class: "_topTitle")
+			}
+			$("./h1[2]"){
+				attributes(class: "_bottomTitle")
+			}
+			$("./h2"){
+				attributes(class: "_bottomTitleSub")
+			}
 
-		$("./h1[1]"){
-			attributes(class: "_topTitle")
-		}
-		$("./h1[2]"){
-			attributes(class: "_bottomTitle")
-		}
-		$("./h2"){
-			attributes(class: "_bottomTitleSub")
-		}
+			$("./h1[@class='_topTitle']"){
+				attributes(data-ur-toggler-component: "button", data-ur-id: "topTitle")
+			}
 
-		$("./h1[@class='_topTitle']"){
-			attributes(data-ur-toggler-component: "button", data-ur-id: "topTitle")
-		}
+			$("./div[@class='brand-gallery']"){
+				attributes(data-ur-toggler-component: "content", data-ur-id: "topTitle")
+			}
 
-		$("./div[@class='brand-gallery']"){
-			attributes(data-ur-toggler-component: "content", data-ur-id: "topTitle")
-		}
-
-		$("./h1[@class='_bottomTitle']"){
-			attributes(data-ur-toggler-component: "button", data-ur-id: "bottomTitle")
-			insert_after("div", id: "_bottomToggler"){
-				attributes(data-ur-toggler-component: "content", data-ur-id: "bottomTitle")
-				move_here("../h2[@class='_bottomTitleSub']")
-				move_here("../div[@class='all-brands-alpha']")
+			$("./h1[@class='_bottomTitle']"){
+				attributes(data-ur-toggler-component: "button", data-ur-id: "bottomTitle")
+				insert_after("div", id: "_bottomToggler"){
+					attributes(data-ur-toggler-component: "content", data-ur-id: "bottomTitle")
+					move_here("../h2[@class='_bottomTitleSub']")
+					move_here("../div[@class='all-brands-alpha']")
+				}
+			}
+			$("./div[@class='all-brands-alpha']"){
+				
 			}
 		}
-
-
-
-
-		$("./div[@class='all-brands-alpha']"){
-			
-		}
-
-
-
-
-
-
-
 	}
 }
--- scripts/pages/products.ts
+++ scripts/pages/products.ts
@@ -1,83 +1,86 @@
 
-# Moves header icons back to the header
-$$("#ad_17601"){
-	move_here("//div[@id='_icons_bar']")
-}
 
-$$(".main_container"){
-	add_class("_products")
+$("/html"){
 
-	$(".//div[@class='search-sidebar']"){
-
-		$("./h2"){
-			insert_after("div", class:"_resultContent"){
-				insert("div", class: "_tabWidget"){
-					move_here("../../ul")
-				}
-			}
-		}
-
-		attribute("data-ur-set", "toggler")
-		$("./h2"){
-			attributes(data-ur-toggler-component: "button", data-ur-toggler-id: "search")
-		}
-		$("./div"){
-			attributes(data-ur-toggler-component: "content", data-ur-toggler-id: "search")
-		}
-
-		$(".//div[@class='_tabWidget']"){
-			attributes(data-ur-set: "tabs", data-ur-closeable: "true")
-			$("ul[1]"){
-				$("./li/h4"){
-					attributes(data-ur-tabs-component: "button", data-ur-tab-id: "price")
-				}
-				$("./li/ul"){
-					attributes(data-ur-tabs-component: "content", data-ur-tab-id: "price")
-				}
-			}
-			$("ul[2]"){
-				$("./li/h4"){
-					attributes(data-ur-tabs-component: "button", data-ur-tab-id: "mfg")
-				}
-				$("./li/ul"){
-					attributes(data-ur-tabs-component: "content", data-ur-tab-id: "mfg")
-				}
-			}
-		}
+	# Moves header icons back to the header
+	$$("#ad_17601"){
+		move_here("//div[@id='_icons_bar']")
 	}
+	$$(".main_container"){
+		add_class("_products")
 
-	$(".//div[@class='search-main']"){
-		$(".//div[@class='search_based']/span[@class='fright']"){
-			$("./select[@id='sortBy']/option"){
-				attribute("value"){
-					value(){
-						rewrite_link()
+		$(".//div[@class='search-sidebar']"){
+
+			$("./h2"){
+				insert_after("div", class:"_resultContent"){
+					insert("div", class: "_tabWidget"){
+						move_here("../../ul")
 					}
 				}
 			}
-			$("./select[@id='orderBy']/option"){
-				attribute("value"){
-					value(){
-						rewrite_link()
+
+			attribute("data-ur-set", "toggler")
+			$("./h2"){
+				attributes(data-ur-toggler-component: "button", data-ur-toggler-id: "search")
+			}
+			$("./div"){
+				attributes(data-ur-toggler-component: "content", data-ur-toggler-id: "search")
+			}
+
+			$(".//div[@class='_tabWidget']"){
+				attributes(data-ur-set: "tabs", data-ur-closeable: "true")
+				$("ul[1]"){
+					$("./li/h4"){
+						attributes(data-ur-tabs-component: "button", data-ur-tab-id: "price")
+					}
+					$("./li/ul"){
+						attributes(data-ur-tabs-component: "content", data-ur-tab-id: "price")
+					}
+				}
+				$("ul[2]"){
+					$("./li/h4"){
+						attributes(data-ur-tabs-component: "button", data-ur-tab-id: "mfg")
+					}
+					$("./li/ul"){
+						attributes(data-ur-tabs-component: "content", data-ur-tab-id: "mfg")
 					}
 				}
 			}
 		}
-		$(".//div[contains(@class, 'result-item')]"){
-			$("./a"){
-				add_class("_productImage")
+
+		$(".//div[@class='search-main']"){
+			$(".//div[@class='search_based']/span[@class='fright']"){
+				$("./select[@id='sortBy']/option"){
+					attribute("value"){
+						value(){
+							rewrite_link()
+						}
+					}
+				}
+				$("./select[@id='orderBy']/option"){
+					attribute("value"){
+						value(){
+							rewrite_link()
+						}
+					}
+				}
 			}
-			$("./div[@class='item-description ']"){
-				move_to("../a", position("before"))
-				insert_after("div", class: "_SKUList", "")
-			}
-			$("./div[@class='item-action ']"){
-				$("./span[@class='discount-price']/div[contains(@class, 'offerprice')]"){
-					$("./span[contains(@class, 'price')]"){
-						match(text()){
-							with(/Call/){
-								$(".."){
-									attributes(style: "margin-bottom: 30px;")
+			$(".//div[contains(@class, 'result-item')]"){
+				$("./a"){
+					add_class("_productImage")
+				}
+				$("./div[@class='item-description ']"){
+					move_to("../a", position("before"))
+					insert_after("div", class: "_SKUList", "")
+				}
+				$("./div[@class='item-action ']"){
+					$("./span[@class='discount-price']/div[contains(@class, 'offerprice')]"){
+						$("./span[contains(@class, 'price')]"){
+							match(text()){
+								with(/Call/){
+									$(".."){
+										attributes(style: "margin-bottom: 30px;")
+									}
 								}
 							}
 						}
@@ -86,13 +89,12 @@ $$(".main_container"){
 			}
 		}
 	}
-}
 
 
 
-$$(".pricingLoginSearch"){
-	$("./a"){
-		attributes(style: "", href: "/webapp/wcs/stores/servlet/AjaxLogonForm?catalogId=11101&myAcctMain=1&langId=-1&storeId=11301", onclick: "")
+	$$(".pricingLoginSearch"){
+		$("./a"){
+			attributes(style: "", href: "/webapp/wcs/stores/servlet/AjaxLogonForm?catalogId=11101&myAcctMain=1&langId=-1&storeId=11301", onclick: "")
+		}
 	}
 }
-
